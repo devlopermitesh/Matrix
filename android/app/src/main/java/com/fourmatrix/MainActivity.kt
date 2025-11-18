dd197b6d1d166685647aec712e35503240603e3c
unknown <gehlotmitesh63@gmail.com>
2025-11-18 18:03:43 +0530

    fix: issue with db test and updated name issue

--- android/app/src/main/java/com/fourmatrix/MainActivity.kt
+++ android/app/src/main/java/com/fourmatrix/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.matrix
+package com.fourmatrix
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -12,7 +12,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "matrix"
+  override fun getMainComponentName(): String = "fourmatrix"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState) // Fixed: pass savedInstanceState instead of null
